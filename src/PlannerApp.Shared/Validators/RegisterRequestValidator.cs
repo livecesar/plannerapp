b5ee20dc66d1b1a2fc86ab91bf4bbfd16edbb642
LiveCesar <livecesar@gmail.com>
2022-04-03 16:40:52 -0500

    RegisterRequest model added; fluent validation.

---
+++ src/PlannerApp.Shared/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using PlannerApp.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Shared.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(p => p.Email)
+                .NotEmpty().WithMessage("Email is mandatory.")
+                .EmailAddress().WithMessage("Not a valid Email");
+
+            RuleFor(p => p.FirstName)
+                .NotEmpty().WithMessage("First name is mandatory")
+                .MaximumLength(30).WithMessage("Name should be 30 character tops.");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("Last name is mandatory")
+                .MaximumLength(30).WithMessage("Last name should be 30 character tops.");
+
+            RuleFor(p => p.Password)
+                .NotEmpty().WithMessage("Password is mandatory").
+                MinimumLength(5);
+
+            RuleFor(p => p.ConfirmPassword)
+                .Equal(p => p.Password).WithMessage("Password does not match.");
+        }
+    }
+}
